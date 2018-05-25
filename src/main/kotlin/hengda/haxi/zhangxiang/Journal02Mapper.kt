d92a2dec1814901f715c9e7902de62e6fe691a0f
limeng <229898977@qq.com>
2018-05-25 10:31:38 +0800

    Revert "李猛"
    This reverts commit dbffea338256f6bf003bef584c139e089787341a.

--- src/main/kotlin/hengda/haxi/zhangxiang/Journal02Mapper.kt
+++ src/main/kotlin/hengda/haxi/zhangxiang/Journal02Mapper.kt
@@ -331,7 +331,6 @@ interface Journal02Mapper {
       date_end = #{dateEnd},
       time_end = #{timeEnd},
       content = #{content},
-      content_detail = #{content_detail},
       p_yq_xdc = #{p_yq_xdc},
       p_yq_jcw = #{p_yq_jcw},
       p_yq_zydd = #{p_yq_zydd},
