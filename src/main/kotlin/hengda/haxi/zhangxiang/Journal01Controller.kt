47f54c823798ddc87e8347ec1dc5780edebb3fb1
ovaphlow <ovaphlow@live.com>
2018-08-11 17:13:47 +0800

    .

--- src/main/kotlin/hengda/haxi/zhangxiang/Journal01Controller.kt
+++ src/main/kotlin/hengda/haxi/zhangxiang/Journal01Controller.kt
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
+/* todo: 移除MyBatis，加入Spring JDBC */
+
 @RestController
 @RequestMapping("/api/journal01")
 class Journal01Controller {
