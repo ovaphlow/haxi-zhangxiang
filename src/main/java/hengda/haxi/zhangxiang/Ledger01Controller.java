e477a3a27e0cafe71e19d741c1e3dc45b971d298
ovaphlow <ovaphlow@live.com>
2018-12-05 17:28:12 +0800

    .

--- src/main/java/hengda/haxi/zhangxiang/Ledger01Controller.java
+++ src/main/java/hengda/haxi/zhangxiang/Ledger01Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
 
-// 禁动牌台�?
+// 禁动牌台帐
 @RequestMapping(value = "/api/ledger/01")
 @RestController
 @SuppressWarnings("unchecked")
@@ -24,39 +24,7 @@ public class Ledger01Controller {
     }
 
     /**
-     *
-     * @return
-     */
-    @GetMapping(value = "/todo/return")
-    public Map<String, Object> todoReturn() {
-        Map<String, Object> resp = new HashMap();
-        try {
-            resp.put("content", repos.returnQty());
-        } catch (Exception e) {
-            logger.error("{}", e);
-            resp.put("message", "服务器错误");
-        }
-        return resp;
-    }
-
-    /**
-     *
-     * @return
-     */
-    @GetMapping(value = "/todo/borrow")
-    public Map<String, Object> todoBorrow() {
-        Map<String, Object> resp = new HashMap();
-        try {
-            resp.put("content", repos.reviewQty());
-        } catch (Exception e) {
-            logger.error("{}", e);
-            resp.put("message", "服务器错误");
-        }
-        return resp;
-    }
-
-    /**
-     *
+     * 统计
      * @return
      */
     @GetMapping(value = "/stats")
@@ -124,83 +92,14 @@ public class Ledger01Controller {
     }
 
     /**
-     * 用户未返还列�?
-     * @param id
+     * 待返还列表
      * @return
      */
-    @GetMapping(value = "/return/user/{id}")
-    public Map<String, Object> listUserReturn(@PathVariable("id") int id) {
-        Map<String, Object> resp = new HashMap();
-        try {
-            resp.put("content", repos.returnListByUser(id));
-        } catch (Exception e) {
-            logger.error("{}", e);
-            resp.put("message", "服务器错误");
-        }
-        return resp;
-    }
-
-    /**
-     * 待返还列�?
-     * @return
-     */
-    @GetMapping(value = "/return")
-    public Map<String, Object> listReturn() {
+    @GetMapping(value = "/return/")
+    public Map<String, Object> listreturn() {
         Map<String, Object> resp = new HashMap();
         try {
             resp.put("content", repos.returnList());
-        } catch (Exception e) {
-            logger.error("{}", e);
-            resp.put("message", "服务器错误");
-        }
-        return resp;
-    }
-
-    /**
-     * 待发放列�?
-     * @return
-     */
-    @GetMapping(value = "/admin")
-    public Map<String, Object> listByAdmin() {
-        Map<String, Object> resp = new HashMap();
-        try {
-            resp.put("content", repos.reviewList());
-        } catch (Exception e) {
-            logger.error("{}", e);
-            resp.put("message", "服务器错误");
-        }
-        return resp;
-    }
-
-    /**
-     * 借出人待发放列表
-     * @param id
-     * @return
-     */
-    @GetMapping(value = "/applicant/{id}")
-    public Map<String, Object> listByApplicant(@PathVariable("id") int id) {
-        Map<String, Object> resp = new HashMap();
-        try {
-            resp.put("content", repos.listByBorrower(id));
-        } catch (Exception e) {
-            logger.error("{}", e);
-            resp.put("message", "服务器错误");
-        }
-        return resp;
-    }
-
-    /**
-     * 发放
-     * @param id
-     * @param body
-     * @return
-     */
-    @PutMapping(value = "/{id}/borrow")
-    public Map<String, Object> borrow(@PathVariable("id") int id, @RequestBody Map<String, Object> body) {
-        Map<String, Object> resp = new HashMap();
-        try {
-            body.put("id", id);
-            repos.borrow(body);
         } catch (Exception e) {
             logger.error("{}", e);
             resp.put("message", "服务器错误");
--- src/main/kotlin/hengda/haxi/zhangxiang/Ledger01Repos.kt
+++ src/main/kotlin/hengda/haxi/zhangxiang/Ledger01Repos.kt
@@ -14,38 +14,16 @@ class Ledger01Repos {
     @Autowired
     private val jdbc: JdbcTemplate? = null
 
-    /**
-     * 计数：待返还
-     */
-    fun returnQty(): Map<String, Any> {
-        return jdbc!!.queryForMap("""
-            select count(*) as qty from journal01 where borrow_id != 0 and return_by_id = 0
-        """.trimIndent())
-    }
-
-    /**
-     * 计数：待发放
-     */
-    fun reviewQty(): Map<String, Any> {
-        return jdbc!!.queryForMap("""
-            select count(*) as qty from journal01 where borrow_date <= '0001-01-01'
-        """.trimIndent())
-    }
-
-    /**
-     * 统计
-     */
+    /** 统计 */
     fun stats(): List<Map<String, Any>> {
-         return jdbc!!.queryForList("""
+        return jdbc!!.queryForList("""
             select applicant as name, count(*) as value from journal01 group by applicant order by value desc
         """.trimIndent())
     }
 
-    /**
-     * 查询
-     */
+    /** 查询 */
     fun filter(body: Map<String, Any>): List<Map<String, Any>> {
-         return jdbc!!.queryForList("""
+        return jdbc!!.queryForList("""
             select
                 *
             from
@@ -57,46 +35,29 @@ class Ledger01Repos {
         """.trimIndent(), body["date"], body["dept"], body["user"])
     }
 
-    /**
-     * 列表
-     */
+    /** 列表 */
     fun list(): List<Map<String, Any>> {
         return jdbc!!.queryForList("""
             select * from journal01 order by id desc limit 200
         """.trimIndent())
     }
 
-    /**
-     * 返还
-     */
+    /** 返还 */
     fun returnHandler(body: Map<String, Any>) {
         jdbc!!.update("""
             update
                 journal01
             set
-                return_name = ?,
                 return_by = ?,
                 return_by_id = ?,
                 return_date = now(),
-                return_time = now(),
-                remark = ?
+                return_time = now()
             where
                 id = ?
-        """.trimIndent(), body["return_name"], body["return_by"], body["return_by_id"], body["remark"], body["id"])
+        """.trimIndent(), body["return_by"], body["return_by_id"], body["id"])
     }
 
-    /**
-     * 指定用户待返还列表
-     */
-    fun returnListByUser(id: Int): List<Map<String, Any>> {
-        return jdbc!!.queryForList("""
-            select * from journal01 where applicant_id = ? and return_by_id != 0
-        """.trimIndent(), id)
-    }
-
-    /**
-     * 所有待返还列表
-     */
+    /** 所有待返还列表 */
     fun returnList(): List<Map<String, Any>> {
         return jdbc!!.queryForList("""
             select
@@ -107,58 +68,11 @@ class Ledger01Repos {
             from
                 journal01
             where
-                borrow_id != 0
-                and return_by_id = 0
+                return_by_id = 0
             order by
                 id desc
             limit 1000
         """.trimIndent())
-    }
-
-    /**
-     * 待发放列表
-     */
-    fun reviewList(): List<Map<String, Any>> {
-         return jdbc!!.queryForList("""
-            select
-                j.id, j.uuid, date, time, quantity, applicant_id, applicant, dept
-            from
-                journal01 as j
-            where
-                borrow_date <= '0001-01-01'
-        """.trimIndent())
-    }
-
-    /**
-     * 借出人待发放列表
-     */
-    fun listByBorrower(id: Int): List<Map<String, Any>> {
-        return jdbc!!.queryForList("""
-            select
-                j.id, j.uuid, date, time, quantity, applicant_id, applicant, dept
-            from
-                journal01 as j
-            where
-                applicant_id = ?
-                and borrow_id = 0
-        """.trimIndent(), id)
-    }
-
-    /**
-     * 发放
-     */
-    fun borrow(body: Map<String, Any>) {
-        jdbc!!.update("""
-            update
-                journal01
-            set
-                borrow_date = now(),
-                borrow_time = now(),
-                borrow = ?,
-                borrow_id = ?
-            where
-                id = ?
-        """.trimIndent(), body["borrow"], body["borrowId"], body["id"])
     }
 
     /**
@@ -170,9 +84,7 @@ class Ledger01Repos {
         """.trimIndent(), id)
     }
 
-    /**
-     * 新增申请
-     */
+    /** 新增申请 */
     fun save(body: Map<String, Any>) {
         jdbc!!.update("""
             insert into
@@ -182,10 +94,10 @@ class Ledger01Repos {
                 date = now(),
                 time = now(),
                 quantity = ?,
-                applicant_id = ?,
                 applicant = ?,
                 dept = ?,
+                borrow = ?,
                 remark = ?
-        """.trimIndent(), body["quantity"], body["applicantId"], body["applicant"], body["dept"], body["remark"])
+        """.trimIndent(), body["quantity"], body["applicant"], body["dept"], body["borrow"], body["remark"])
     }
 }
